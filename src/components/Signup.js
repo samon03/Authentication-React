b13e667313229d466f35bb45de35659a71495edd
samon03 <samon3123@gmail.com>
2021-10-26 15:35:59 +0600

    sign up with useContext

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,17 +1,40 @@
-import React, { useRef } from 'react';
-import { Card, Form, Button } from 'react-bootstrap';
+import React, { useRef, useState } from 'react';
+import { Card, Form, Button, Alert } from 'react-bootstrap';
+import { useAuth } from '../Contexts/AuthContext';
 
 export default function Signup() {
     const emailRef = useRef();
     const passwordRef = useRef();
     const confirmPasswordRef = useRef();
+    const { signup } = useAuth();
+    const [error, setError] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    async function handleSubmit(e) {
+       e.preventDefault();
+
+       if(passwordRef.current.value !== confirmPasswordRef.current.value) {
+           return setError('Password not matched');
+       }
+
+       try {
+          setError('');
+          setLoading(true);
+          await signup(emailRef.current.value, passwordRef.current.value);
+       } catch {
+           return setError('Failed to create an account');
+       }
+
+       setLoading(false);
+    }
 
     return (
         <>
             <Card className="p-3">
                 <h2 className="text-center mb-4"> Sign Up</h2>
+                {error && <Alert variant="danger">{error}</Alert>}
                <Card.Body>
-                    <Form>
+                    <Form onSubmit={handleSubmit}>
                         <Form.Group className="mb-3" id="email">
                             <Form.Label>Email address</Form.Label>
                             <Form.Control type="email" ref={emailRef} placeholder="Enter email" required />
@@ -25,7 +48,7 @@ export default function Signup() {
                             <Form.Label>Confirm Password</Form.Label>
                             <Form.Control type="password" ref={confirmPasswordRef} placeholder="Enter Confirm Password" required />
                         </Form.Group>
-                        <Button className="w-100 mb-3" type="submit">
+                        <Button disabled={loading} className="w-100 mb-3" type="submit">
                             Sign Up
                         </Button>
                     </Form>
