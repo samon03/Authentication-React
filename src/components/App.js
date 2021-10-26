edf2899f2d30b3134362e2c2a979b357edbd1168
samon03 <samon3123@gmail.com>
2021-10-26 22:21:10 +0600

    private router

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Signup from './Signup';
 import Dashboard from './Dashboard';
 import Login from './Login';
+import PrivateRoute from './PrivateRoute';
 
 function App() {
   return (
@@ -15,7 +16,7 @@ function App() {
                 <Router>
                    <AuthProvider>
                       <Switch>
-                        <Route exact path="/" component={Dashboard} />
+                        <PrivateRoute exact path="/" component={Dashboard} />
                         <Route path="/signup" component={Signup} />
                         <Route path="/login" component={Login} />
                       </Switch>
